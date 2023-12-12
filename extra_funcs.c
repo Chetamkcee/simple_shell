2fce5d22fceecd74f33431bc33a779baa6ad4f36
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-12-12 23:55:09 +0100

    added lines in the extra_funcs for extra functions

--- extra_funcs.c
+++ extra_funcs.c
@@ -23,3 +23,41 @@ char * getline(void)
         }
         return lineptr;
 }
+
+/**
+ * @brief Function to handle the exit command in the shell
+ * @param arguments Command arguments
+ * @param input String from standard input
+ * @param exit_code Exit code value
+ * @return None
+ */
+void handle_exit_command(char **arguments, char *input, int exit_code) {
+    int status = 0;
+
+    if (!arguments[1]) {
+        free(input);
+        free(arguments);
+        exit(exit_code);
+    }
+    status = convert_to_integer(arguments[1]);
+
+    free(input);
+    free(arguments);
+    exit(status);
+}
+
+/**
+ * @brief Function to retrieve and display environment variables
+ * @param environment Environment variables
+ * @return None
+ */
+void display_environment(char **environment) {
+    size_t index = 0;
+
+    while (environment[index]) {
+        write_output(STDOUT_FILENO, environment[index], string_length(environment[index]));
+        write_output(STDOUT_FILENO, "\n", 1);
+        index++;
+    }
+}
+
