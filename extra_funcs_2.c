657502a0afbe1d750627f04acbb917bb95025d14
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-12-13 14:29:42 +0100

    solved an error

--- extra_funcs_2.c
+++ extra_funcs_2.c
@@ -1,31 +1,35 @@
 #include "shell.h"
-#include <stddef.h>
 
 /**
-* myStrspn - Gets the length of a prefix substring.
-* @str1: String to be scanned.
-* @str2: String containing the characters to match.
-* Return: The number of characters in the initial segment of str1 which
-* consist only of characters from str2.
-*/
-size_t myStrspn(const char *str1, const char *str2){
-  size_t i,j;
-  i=0;
+ * myStrspn - Gets the length of a prefix substring.
+ * @str1: String to be scanned.
+ * @str2: String containing the characters to match.
+ * Return: The number of characters in the initial segment of str1 which
+ * consist only of characters from str2.
+ */
+size_t myStrspn(const char *str1, const char *str2)
+{
+	size_t i, j;
+	i = 0;
 
-  while(*(str1+i)){
-    j=0;
-    while(*(str2+j)){
-      if(*(str1+i) == *(str2+j)){
-        break; //Found a match.
-      }
-      j++;
-    }
-    if(!*(str2+j)){
-      return i; //No match found.
-    }
-    i++;
-  }
-  return i;
+	while (*(str1 + i))
+	{
+		j = 0;
+		while (*(str2 + j))
+		{
+			if (*(str1 + i) == *(str2 + j))
+			{
+				break; // Found a match.
+			}
+			j++;
+		}
+		if (!*(str2 + j))
+		{
+			return i; // No match found.
+		}
+		i++;
+	}
+	return i;
 }
 /**
  * _strchr - locates the ﬁrst occurrence of c (converted to a char) in the
@@ -45,21 +49,24 @@ char *_strchr(const char *s, int c)
 }
 
 /**
- * _strcspn - Finds the length of the segment in 's1' that does not contain any characters from 's2'.
+ * _strcspn - Finds the length of the segment in 's1'
+ * that does not contain any characters from 's2'.
  * @s1 The string to check
  * @s2 The string used for comparison
  * return The length of the segment
  */
-size_t _strcspn(const char *s1, const char *s2) {
-    size_t ret = 0;
+size_t _strcspn(const char *s1, const char *s2)
+{
+	size_t ret = 0;
 
-    while (*s1) {
-        if (_strchr(s2, *s1))
-            return ret;
-        s1++;
-        ret++;
-    }
-    return ret;
+	while (*s1)
+	{
+		if (_strchr(s2, *s1))
+			return ret;
+		s1++;
+		ret++;
+	}
+	return ret;
 }
 
 /**
@@ -68,31 +75,39 @@ size_t _strcspn(const char *s1, const char *s2) {
  * @delim The delimiter string
  * return The first/next token if possible, NULL otherwise
  */
-char *_strtok(char *str, const char *delim) {
-    static char *p;
+char *_strtok(char *str, const char *delim)
+{
+	static char *p;
 
-    if (str) {
-        p = str;
-    } else if (!p) {
-        return NULL;
-    }
+	if (str)
+	{
+		p = str;
+	}
+	else if (!p)
+	{
+		return NULL;
+	}
 
-    str = p + myStrspn(p, delim);
-    p = str + _strcspn(str, delim);
-    
-    if (p == str) {
-        p = NULL;
-        return NULL;
-    }
+	str = p + myStrspn(p, delim);
+	p = str + _strcspn(str, delim);
 
-    if (*p != '\0') {
-        *p = '\0';
-        p++;
-    } else {
-        p = NULL;
-    }
+	if (p == str)
+	{
+		p = NULL;
+		return NULL;
+	}
 
-    return str;
+	if (*p != '\0')
+	{
+		*p = '\0';
+		p++;
+	}
+	else
+	{
+		p = NULL;
+	}
+
+	return str;
 }
 
 /**
@@ -100,54 +115,52 @@ char *_strtok(char *str, const char *delim) {
  * @command_line: User command
  * Return: Pointer to tokens
  */
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include "shell.h"
+char **get_tokens(char *command_line)
+{
+	if (command_line == NULL)
+		return NULL;
 
-char **get_tokens(char *command_line) {
-    if (command_line == NULL)
-        return NULL;
+	// Count the number of tokens
+	size_t token_count = 0;
+	char *copy = strdup(command_line);
+	if (copy == NULL)
+		return NULL;
 
-    // Count the number of tokens
-    size_t token_count = 0;
-    char *copy = strdup(command_line);
-    if (copy == NULL)
-        return NULL;
+	char *token = _strtok(copy, " \n\t\r");
+	while (token != NULL)
+	{
+		token_count++;
+		token = _strtok(NULL, " \n\t\r");
+	}
 
-    char *token = _strtok(copy, " \n\t\r");
-    while (token != NULL) {
-        token_count++;
-        token = _strtok(NULL, " \n\t\r");
-    }
+	char **user_command = malloc((token_count + 1) * sizeof(char *));
+	if (user_command == NULL)
+	{
+		free(copy);
+		return NULL;
+	}
 
-    // Allocate memory for an array of pointers
-    char **user_command = malloc((token_count + 1) * sizeof(char *));
-    if (user_command == NULL) {
-        free(copy);
-        return NULL;
-    }
+	token = _strtok(copy, " \n\t\r");
 
-    // Reset command_line and token
-    token = _strtok(copy, " \n\t\r");
+	size_t i = 0;
+	while (token != NULL && i < token_count)
+	{
+		user_command[i] = strdup(token);
+		if (user_command[i] == NULL)
+		{
+			for (size_t j = 0; j < i; j++)
+			{
+				free(user_command[j]);
+			}
+			free(user_command);
+			free(copy);
+			return NULL;
+		}
+		token = _strtok(NULL, " \n\t\r");
+		i++;
+	}
+	user_command[i] = NULL;
 
-    size_t i = 0;
-    while (token != NULL && i < token_count) {
-        user_command[i] = strdup(token); // Allocate memory for each token
-        if (user_command[i] == NULL) {
-            // Handle memory allocation failure
-            for (size_t j = 0; j < i; j++) {
-                free(user_command[j]);
-            }
-            free(user_command);
-            free(copy);
-            return NULL;
-        }
-        token = _strtok(NULL, " \n\t\r");
-        i++;
-    }
-    user_command[i] = NULL;
-
-    free(copy); // Free the copied command_line string
-    return user_command;
+	free(copy);
+	return user_command;
 }
