c5abb32ac33af130e87e1f6cd16ff3b9a7ffd8ae
Chetamkcee <agbodikechetty@gmail.com>
2023-12-13 13:01:47 +0100

    simpleshell

--- extra_funcs.c
+++ extra_funcs.c
@@ -1,61 +1,67 @@
 #include "shell.h"
-
-/** 
- * getline - function that returns the a line passed to the interpreter
+/**
+ * read_command - function that returns the line passed to the interpreter
  *
  * Return: a pointer to the buffer
  */
 char *read_command(void)
 {
-        size_t n = 0;
-        char *lineptr = NULL;
-        ssize_t read_result;
+	size_t n = 0;
+	char *lineptr = NULL;
+	ssize_t read_result;
 
-        if (isatty(STDIN_FILENO))
-                write(STDOUT_FILENO, "$ ", 2);
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
 
-        read_result = getline(&lineptr, &n, stdin);
-        if (!read_result)
-        {
-                free(lineptr);
-                return(NULL);
-        }
-        return lineptr;
+	read_result = getline(&lineptr, &n, stdin);
+	if (!read_result)
+	{
+		free(lineptr);
+		return (NULL);
+	}
+
+	return (lineptr);
 }
 
 /**
  * exit_command - Function to handle the exit command in the shell
- * @arguments Command arguments
- * @input String from standard input
- * @exit_code Exit code value
+ * @arguments: Command arguments
+ * @input: String from standard input
+ * @exit_code: Exit code value
  * Return: None
  */
-void exit_command(char **arguments, char *input, int exit_code) {
-    int status = 0;
+void exit_command(char **arguments, char *input, int exit_code)
+{
+	int status = 0;
 
-    if (!arguments[1]) {
-        free(input);
-        free(arguments);
-        exit(exit_code);
-    }
-    status = atoi(arguments[1]);
+	if (!arguments[1])
+	{
+		free(input);
+		free(arguments);
+		exit(exit_code);
+	}
 
-    free(input);
-    free(arguments);
-    exit(status);
+	status = atoi(arguments[1]);
+
+	free(input);
+	free(arguments);
+	exit(status);
 }
 
 /**
  * display_environment - Function to retrieve and display environment variables
- * @environment Environment variables
+ * @environment: Environment variables
  * Return: None
  */
-void display_environment(char **environment) {
-    size_t index = 0;
+void display_environment(char **environment)
+{
+	size_t index = 0;
 
-    while (environment[index]) {
-        write(STDOUT_FILENO, environment[index], string_length(environment[index]));
-        write(STDOUT_FILENO, "\n", 1);
-        index++;
-    }
+	while (environment[index])
+	{
+		write(STDOUT_FILENO, environment[index], string_length(environment[index]));
+		write(STDOUT_FILENO, "\n", 1);
+		index++;
+	}
 }
+
--- extra_funcs_2.c
+++ extra_funcs_2.c
@@ -2,152 +2,107 @@
 #include <stddef.h>
 
 /**
-* myStrspn - Gets the length of a prefix substring.
-* @str1: String to be scanned.
-* @str2: String containing the characters to match.
-* Return: The number of characters in the initial segment of str1 which
-* consist only of characters from str2.
-*/
-size_t myStrspn(const char *str1, const char *str2){
-  size_t i,j;
-  i=0;
-
-  while(*(str1+i)){
-    j=0;
-    while(*(str2+j)){
-      if(*(str1+i) == *(str2+j)){
-        break; //Found a match.
-      }
-      j++;
-    }
-    if(!*(str2+j)){
-      return i; //No match found.
-    }
-    i++;
-  }
-  return i;
-}
-/**
- * _strchr - locates the ﬁrst occurrence of c (converted to a char) in the
- * string pointed to by s. The terminating null character is considered to be
- * part of the string.
- * @s: string
- * @c: character
- * Return: a pointer to the located character, or a null pointer
- * if the character does not occur in the string.
- **/
-char *_strchr(const char *s, int c)
+ * string_concatenate - Concatenates src string to the end of dest string.
+ * @dest: Destination string
+ * @src: Source string
+ * Return: Pointer to the destination string
+ */
+char *string_concatenate(char *dest, char *src)
 {
-	while (*s != (char)c)
-		if (!*s++)
-			return (0);
-	return ((char *)s);
+	int dest_index = 0;
+	int src_index = 0;
+
+	while (dest[dest_index] != '\0')
+	{
+		dest_index++;
+	}
+	while (src[src_index] != '\0')
+	{
+		dest[dest_index] = src[src_index];
+		dest_index++;
+		src_index++;
+	}
+	dest[dest_index] = '\0';
+	return dest;
 }
 
 /**
- * _strcspn - Finds the length of the segment in 's1' that does not contain any characters from 's2'.
- * @s1 The string to check
- * @s2 The string used for comparison
- * return The length of the segment
+ * string_compare - Compares two strings.
+ * @s1: First string
+ * @s2: Second string
+ * Return: Integer indicating the comparison result
  */
-size_t _strcspn(const char *s1, const char *s2) {
-    size_t ret = 0;
+int string_compare(char *s1, char *s2)
+{
+	int index;
 
-    while (*s1) {
-        if (_strchr(s2, *s1))
-            return ret;
-        s1++;
-        ret++;
-    }
-    return ret;
+	for (index = 0; s1[index] != '\0' && s2[index] != '\0'; index++)
+	{
+		if (s1[index] != s2[index])
+		{
+			return ((int)s1[index] - s2[index]);
+		}
+	}
+	return 0;
 }
 
 /**
- * _strtok - Tokenizes a string based on the given delimiter.
- * @str The string to tokenize
- * @delim The delimiter string
- * return The first/next token if possible, NULL otherwise
+ * string_length - Calculates the length of a string.
+ * @s: Pointer to the string
+ * Return: Length of the string
  */
-char *_strtok(char *str, const char *delim) {
-    static char *p;
+int string_length(char *s)
+{
+	int length = 0;
 
-    if (str) {
-        p = str;
-    } else if (!p) {
-        return NULL;
-    }
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
 
-    str = p + myStrspn(p, delim);
-    p = str + _strcspn(str, delim);
-    
-    if (p == str) {
-        p = NULL;
-        return NULL;
-    }
-
-    if (*p != '\0') {
-        *p = '\0';
-        p++;
-    } else {
-        p = NULL;
-    }
-
-    return str;
+	return length;
 }
 
 /**
- * get_tokens - Get tokens from a string
- * @command_line: User command
- * Return: Pointer to tokens
+ * string_compare_n - Compares the first n characters of two strings.
+ * @s1: First string
+ * @s2: Second string
+ * @n: Number of characters to compare
+ * Return: Difference between the strings
  */
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include "shell.h"
+size_t string_compare_n(char *s1, char *s2, size_t n)
+{
+	size_t index, diff;
 
-char **get_tokens(char *command_line) {
-    if (command_line == NULL)
-        return NULL;
+	for (index = 0; s1[index] != '\0' && index < n; index++)
+	{
+		diff = s1[index] - s2[index];
 
-    // Count the number of tokens
-    size_t token_count = 0;
-    char *copy = strdup(command_line);
-    if (copy == NULL)
-        return NULL;
-
-    char *token = _strtok(copy, " \n\t\r");
-    while (token != NULL) {
-        token_count++;
-        token = _strtok(NULL, " \n\t\r");
-    }
-
-    // Allocate memory for an array of pointers
-    char **user_command = malloc((token_count + 1) * sizeof(char *));
-    if (user_command == NULL) {
-        free(copy);
-        return NULL;
-    }
-
-    // Reset command_line and token
-    token = _strtok(copy, " \n\t\r");
-
-    size_t i = 0;
-    while (token != NULL && i < token_count) {
-        user_command[i] = strdup(token); // Allocate memory for each token
-        if (user_command[i] == NULL) {
-            // Handle memory allocation failure
-            for (size_t j = 0; j < i; j++) {
-                free(user_command[j]);
-            }
-            free(user_command);
-            free(copy);
-            return NULL;
-        }
-        token = _strtok(NULL, " \n\t\r");
-        i++;
-    }
-    user_command[i] = NULL;
-
-    free(copy); // Free the copied command_line string
-    return user_command;
+		if (diff != 0)
+		{
+			return diff;
+		}
+	}
+	return 0;
 }
+
+/**
+ * string_copy - Copies the source string to the destination string.
+ * @dest: Destination string
+ * @src: Source string
+ * Return: Pointer to the destination string
+ */
+char *string_copy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		++i;
+	}
+	*(dest + i) = *(src + i);
+
+	return dest;
+}
+
